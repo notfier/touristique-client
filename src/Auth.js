6fc3a7d0fb510efe5633ad7867b0664473407aa0
notfier <notfier@gmail.com>
2016-12-25 23:57:27 +0200

    added login page and default structure, connect bootstrap

--- src/Auth.js
+++ src/Auth.js
@@ -2,76 +2,85 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Link } from 'react-router';
 
+
 // components
 
 
 export class Auth extends Component {
 
-    constructor( props ) {
-        super( props );
-        this.state = {
-            usernameValue: '',
-            email: '',
-            password: ''
-        };
-        this.handleChange = this.handleChange.bind( this );
-        this.onUsernameUpdate = this.onUsernameUpdate.bind( this );
-        this.onPasswordUpdate = this.onPasswordUpdate.bind( this );
-        this.onEmailUpdate = this.onEmailUpdate.bind( this );
-    }
+  constructor( props ) {
+    super( props );
+    this.state = {
+      touristCardId: '',
+    };
+  }
 
-    onUsernameUpdate( val ) {
-        this.setState({
-            usernameValue: val
-        });
-    }
-
-    onEmailUpdate( val ) {
-        this.setState({
-            email: val
-        });
-    }
-
-    onPasswordUpdate( val ) {
-        console.log( 'yess', val );
-        this.setState({
-            password: val
-        });
-    }
-
-    handleChange( event ) {
-        localStorage.setItem( 'token', JSON.stringify({
-            'username': this.state.usernameValue,
-            'email': this.state.email,
-            'password': this.state.password
-        }) );
-    }
-
-    render() {
-        return (
-            <div className="entry">
-                <div className="title">
-                    <h2>Barca DepSystem</h2>
-                </div>
-                <div className="form">
-                    <form>
-                        <div className="inner-form">
-                            <input type='text' onChange={ this.onUsernameUpdate }/>
-                        </div>
-                        {/* use only username for authentication now */}
-                        <div className="inner-form">
-                            <Link to="/">
-                                <input
-                                    id="sign-in"
-                                    type="button"
-                                    value="Sign In"
-                                    onClick={ this.handleChange }
-                                ></input>
-                            </Link>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <h2 className="text-center">Barca DepSystem</h2>
+        <div className="container">
+          <div className="col-xs-6">
+            <h3 className="text-center">Tourist</h3>
+            <form className="inner-form center-block" autoComplete='off'>
+              <div className="form-group">
+                <input
+                  type='text'
+                  id='cc1'
+                  placeholder='Enter your tourist card id'
+                  onChange={ () => { alert('Not implemented yet!') } }
+                  className="form-control"
+                />
+              </div>
+              {/* use only username for authentication now */}
+              <div className="form-group">
+                <Link to="/">
+                  <input
+                    type="button"
+                    value="Sign In as a Tourist"
+                    className="btn btn-default form-control btn-warning"
+                    onClick={ () => { alert('Not implemented yet!') } }
+                  ></input>
+                </Link>
+              </div>
+            </form>
+          </div>
+          <div className="col-xs-6">
+            <h3 className="text-center">Operator</h3>
+            <form className="inner-form center-block" autoComplete='off'>
+              <div className="form-group">
+                <input
+                  type='text'
+                  id='cc2'
+                  placeholder='Enter your username(AKA email)'
+                  onChange={ () => { alert('Not implemented yet!') } }
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type='password'
+                  id='cc3'
+                  placeholder='Enter your operator password'
+                  onChange={ () => { alert('Not implemented yet!') } }
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <Link to="/">
+                  <input
+                    id="sign-in"
+                    type="button"
+                    value="Sign In as an Operator"
+                    className="btn btn-default form-control btn-warning"
+                    onClick={ () => { alert('Not implemented yet!') } }
+                  ></input>
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+var css = require('./styles/main.css');
+
 import React from 'react';
 import { render } from 'react-dom';
 import { IndexRoute, Router, Route, browserHistory } from 'react-router';
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 import { App } from './App';
 import { Auth } from './Auth';
